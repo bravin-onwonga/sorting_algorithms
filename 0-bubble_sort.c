1d230a0a39ee91911bca8188744f84e84eacfe8c
bravin-onwonga <onbravin@gmail.com>
2024-02-17 00:07:12 +0300

    Algorithm similar to bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting algorithm similar to bubble sort
+ *
+ * @array: array of integers
+ * @size: number of elements in array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, k = 0;
+	int count, temp;
+
+	for (k = 0; k < size; k++)
+	{
+		count = 0;
+		for (j = 1; j < size; j++)
+		{
+			i = j - 1;
+			if (array[i] > array[j])
+			{
+				temp = array[j];
+				array[j] = array[i];
+				array[i] = temp;
+				count++;
+				print_array(array, size);
+			}
+		}
+		if (count == 0)
+			break;
+	}
+}
