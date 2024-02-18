5b432e28a44796bb10b9c596ffecc5cea75e99b7
bravin-onwonga <onbravin@gmail.com>
2024-02-19 01:08:53 +0300

    Insertion sort look alike

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort a list with the insertion sort algorithm
+ *
+ * @list: doubly-linked list head node
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *curr;
+	int i, n, k = 1;
+
+	curr = *list;
+	temp = NULL;
+	while (curr)
+	{
+		n = curr->n;
+		i = 0;
+		temp = curr->next;
+		while (i < k)
+		{
+			if (n < temp->n)
+			{
+				curr->prev->next = curr->next;
+				curr->prev = temp->prev;
+				curr->prev->next = curr;
+				temp->prev = curr;
+				curr->next = temp;
+				print_list(*list);
+				break;
+			}
+			i++;
+			temp = temp->next;
+		}
+		k++;
+		curr = curr->next;
+	}
+	print_list(*list);
+}
